00f7fd7d496675c1b9b1ee6f5a6ea87c253ad4b6
Jack <jiajie.tan@midea.com>
2022-01-20 18:18:01 +0800

    arr obj

--- defineProperty/defineProperty.js
+++ defineProperty/defineProperty.js
@@ -1,7 +1,12 @@
 let vue = {}
 
 let data = {
-    data: 1
+    data: 1,
+    arr: [1,2,3,4,5],
+    obj: {
+        a: 1,
+        b: 2
+    }
 }
 
 function dataEmitter(value) {
@@ -20,5 +25,42 @@ Object.defineProperty(vue, "data", {
     configurable : true
 })
 
+Object.defineProperty(vue, "arr", {
+    get() {
+        return data.arr;
+    },
+    set(value) {
+        data.arr = value;
+        dataEmitter(value);
+    },
+    enumerable : true,
+    configurable : true
+})
+
+Object.defineProperty(vue, "obj", {
+    get() {
+        return data.obj;
+    },
+    set(value) {
+        data.obj = value;
+        dataEmitter(value);
+    },
+    enumerable : true,
+    configurable : true
+})
+
 vue.data = 2;
-console.log(vue.data)
+vue.arr = [2,3,4,5,6]
+console.log(vue.arr)
+vue.arr.push(9);
+console.log(vue.arr)
+vue.arr[0] = 100;
+console.log(vue.arr)
+
+console.log('------------')
+vue.obj.a = 'good'
+console.log(vue.obj)
+Object.assign(vue.obj, {c: 'bad'})
+console.log(vue.obj)
+vue.obj = Object.assign({}, {...vue.obj, c: 'bad'})
+console.log(vue.obj)
