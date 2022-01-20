0360748ca432915a98b891cd9245b4074113d239
Jack <jiajie.tan@midea.com>
2022-01-20 17:52:57 +0800

    567

---
+++ leetcode/567.js
@@ -0,0 +1,14 @@
+/**
+ * 给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。如果是，返回 true ；否则，返回 false 。
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+ var checkInclusion = function(s1, s2) {
+    
+};
+
+// s1 = "ab" s2 = "eidbaooo"
+
+console.log(checkInclusion("ab", "eidbaooo"))
+console.log(checkInclusion("ab", "eidboaoo"))
