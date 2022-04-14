29bc6f085803368865b401aff8900f64d9e4ae78
Jack <jiajie.tan@midea.com>
2022-04-14 17:25:35 +0800

    过辣！

--- leetcode/62.js
+++ leetcode/62.js
@@ -3,6 +3,7 @@
  *
  * [62] 不同路径
  */
+let map = new Map();
 
 // @lc code=start
 /**
@@ -13,13 +14,19 @@
 var uniquePaths = function(m, n) {
     if(m == 0 || n == 0) return 0
     if(m == 1 && n == 1) return 1;
-    if((m == 2 && n == 1) || (m == 1 && n == 2)) return 1;
 
-    let result = uniquePaths(m-1, n) + uniquePaths(m, n-1)
-    return result;
+    let key = `${m}_${n}`;
+    if(map.has(key)) {
+        return map.get(key)
+    } else {
+        let result = uniquePaths(m-1, n) + uniquePaths(m, n-1)
+        map.set(key, result)
+        return result;
+    }
 };
 // @lc code=end
 
 console.log(uniquePaths(3,2))
 console.log(uniquePaths(3,3))
 console.log(uniquePaths(7,3))
+console.log(uniquePaths(51,9))
