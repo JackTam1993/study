2d27239d2ce1ef2c83ffc466e1f3799a49ad69de
Jack <jiajie.tan@midea.com>
2022-04-24 18:58:29 +0800

    过辣

--- leetcode/209.js
+++ leetcode/209.js
@@ -15,28 +15,31 @@
     let right = 0;
     let length = nums.length;
     let sum = nums[0];
-    let result = 1000;
+    let result = 10000000;
 
     while(right < length) {
         if(sum < target) {
             right++;
             sum += nums[right]
-        } else if(sum > target) {
-            sum -= nums[left]
-            left++;
-        } else {
-            if(right - left == 0) {
-                //
+        } else if(sum >= target) {
+            result = Math.min(result, right - left + 1);
+            
+            // 判断一下，减少left是否仍然大于target，是的话就减，不是的话就加
+            if(sum - nums[left] >= target) {
+                sum -= nums[left]
+                left++
             } else {
-                result = Math.min(result, right - left);
+                right++
+                sum += nums[right]
             }
-            right++;
         }
     }
-    return result == 1000 ? 0 : result;
+    return result == 10000000 ? 0 : result;
 };
 // @lc code=end
 
-// console.log(minSubArrayLen(7, [2,3,1,2,4,3]))
+console.log(minSubArrayLen(7, [2,3,1,2,4,3]))
 console.log(minSubArrayLen(4, [1,4,4]))
 console.log(minSubArrayLen(1, [1,1,1,1,1,1,1,1]))
+console.log(minSubArrayLen(11, [1,2,3,4,5]))
+console.log(minSubArrayLen(15, [1,2,3,4,5]))
