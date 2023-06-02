eae51401169e4b4e45fc455df3ceaeb8b349ae26
tanjj4 <jiajie.tan@midea.com>
2023-06-02 15:02:37 +0800

    新方法

--- leetcode/14.js
+++ leetcode/14.js
@@ -7,25 +7,54 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-    let prefix = ''
+    // let prefix = ''
 
-    for (let i = 0; i < strs[0].length; i++) {
-        for (let j = 0; j < strs.length; j++) {
+    // for (let i = 0; i < strs[0].length; i++) {
+    //     for (let j = 0; j < strs.length; j++) {
 
-            let temp_prefix = strs[0].slice(0, i + 1);
-            console.log(temp_prefix)
-            if (!strs[j].slice(0, i + 1).includes(temp_prefix)) {
-                break;
-            } else if (j == strs.length - 1) {
-                // 最后一个了
-                prefix = temp_prefix
-             }
+    //         let temp_prefix = strs[0].slice(0, i + 1);
+    //         console.log(temp_prefix)
+    //         if (!strs[j].slice(0, i + 1).includes(temp_prefix)) {
+    //             break;
+    //         } else if (j == strs.length - 1) {
+    //             // 最后一个了
+    //             prefix = temp_prefix
+    //          }
+    //     }
+    // }
+    if(strs.length == 1) return strs[0];
+    let prefix = '';
+    let map = new Map();
+    
+    // 排序
+    strs = strs.sort((a, b) => a.length - b.length);
+    let pointer = strs[0].length;
+    while(pointer >= 0) {
+
+        for(let i = 0; i < strs.length; i++) {
+            let slice = strs[i].slice(0, pointer)
+            map.set(slice, slice)
+        }
+
+        // 如果map的长度超过1，就是有不一样的
+        if(map.size > 1) {
+            pointer--;
+            map.clear();
+        } else {
+            break;
         }
     }
+
+    prefix = strs[0].slice(0, pointer)
 
     return prefix;
 
 };
 
 // fl
-console.log(longestCommonPrefix(["c","acc","ccc"]))
+console.log(longestCommonPrefix(["flower","flower","flower","flower"]))
+console.log(longestCommonPrefix(["flow","flower","flight"]))
+console.log(longestCommonPrefix(["a","ab"]))
+console.log(longestCommonPrefix(["a"]))
+console.log(longestCommonPrefix([""]))
+console.log(longestCommonPrefix(["a", "b"]))
